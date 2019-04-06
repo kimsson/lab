88ea6fa028414e44ba819645f46f59959d3b0850
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2019-04-06 18:00:10 +0200

    nasa image of the day by date

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import './App.css';
-import ClassNames from 'classnames';
-import Sound from 'react-sound';
+// import ClassNames from 'classnames';
+// import Sound from 'react-sound'; 
 
-class App extends Component {
+export default function App(props) {
   
-  constructor (props) {
-    super(props);
-  }
-  
-  render() {
-    const renderContent = () => this.state.content.split(" ");
+  const API_KEY = 'Nv6JENKOagk7nHmHTpKBfqXKk5z4PUtRs1nCAhRU'; // fix me
 
-    const appClassName = ClassNames({
-      'App': true,
-      'App-fadeIn': true,
-    })
-    const linkClassName = ClassNames({
-      'App-link': true,
-      'App-link-hidden': true,
-    })
-    
-    return (
-      <div className={appClassName}>
-        <header className="App-header App-noselect">
-          <h1>Hello Dolly</h1>
-        </header>
+  const [query, setQuery] = useState('redux');
+  const [date, setDate] = useState(new Date());
+
+
+  const [data, setData] = useState({item: {}});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      const url = `Https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&hd=true&date=${moment(date).format('YYYY-MM-DD')}`;
+      console.log('url', url);
+      try {
+        const result = await axios(
+          url,
+          );
+          setData(result.data);
+      } catch(error) {
+        setIsError(true)
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [date]);
+  
+
+  return (
+    <div className="App">
+      <div className="App-info">
+        {/* <input
+            type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)} 
+          /> */}
+          <DayPicker
+            onDayClick={(event) => setDate(event)}/>
+          {console.log('isError',isError)}
+          {console.log('isLoading',isLoading)}
+          {isError && <div>Something went wrong ...</div>}
+        </div>
+      <div className="App-header">
+        { isLoading ? (<div>Loading ...</div>) : (
+          <img src={data.url} alt={data.explanation}
+          />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
